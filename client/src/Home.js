ca50c912c33ad103df253be2114e51e848747774
Juwile <nadine.muelhauser@gmail.com>
2020-10-17 12:17:24 +0200

    diamond implemented and teaser

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import img from './piano.JPG'
 
 export default function Home() {
     return(
         <div>
-            <h2>This is a placeholder for Home</h2>
+        <main>
+            <article className="rectangle home">
+                <h1>Piano und Gesang</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit a.</p>
+                <div class="teaser-container">
+                  <div class="teaser">
+                    <div class="diamond">
+                      <img src={img} alt="avatar"></img>
+                    </div>
+                    <h2>Teaser 1</h2>
+                    <p class="teaser-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                  <div class="teaser">
+                    <div class="diamond">
+                      <img src={img} alt="avatar"></img>
+                    </div>
+                    <h2>Teaser 1</h2>
+                    <p class="teaser-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                  <div class="teaser">
+                    <div class="diamond">
+                      <img src={img} alt="avatar"></img>
+                    </div>
+                    <h2>Teaser 1</h2>
+                    <p class="teaser-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                </div>
+            </article>
+        </main>
         </div>
     );
 }
