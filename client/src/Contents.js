f0efe3f88979f1a80f6052b7fe949c48177371ce
Juwile <nadine.muelhauser@gmail.com>
2020-10-31 15:47:54 +0100

    env file home anpassung

--- client/src/Contents.js
+++ client/src/Contents.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './Home.js';
 import Repertoire from './Repertoire.js';
+import Kontakt from './Kontakt.js';
 
 const NotFound = () => <h1>Page Not Found</h1>;
 
@@ -12,6 +13,7 @@ export default function Contents() {
             <Redirect exact from="/" to="/home"/>
             <Route path="/home" component={Home} />
             <Route path="/repertoire" component={Repertoire} />
+            <Route path="/kontakt" component={Kontakt} />
             <Route component={NotFound} />
         </Switch>
     );
