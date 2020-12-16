a3c664e25e064616ffd54801f950932d3f088b55
Juwile <nadine.muelhauser@gmail.com>
2020-12-16 19:09:20 +0100

    background img fixed

--- client/src/Links.js
+++ client/src/Links.js
@@ -8,13 +8,13 @@ export default function Links() {
             <main>
                 <article className="rectangle links">
                     <h1>Wir empfehlen...</h1>
-                    <h2>Musik</h2>
-                    <p><a href="http://www.airwinsbigband.com/">AIRWIN's Big Band</a></p>
-                    <p><a href="http://www.blaskapelle-nordsued.com/">Blaskapelle Nord Süd</a></p>
                     <h2>Zeremonie</h2>
                     <a href="http://www.amoroso-vivace.ch/">Amoroso-Vivace</a>
                     <h2>Fotografie</h2>
                     <a href="https://pascal-gugler.ch/">Pascal Gugler</a>
+                    <h2>Musik</h2>
+                    <p><a href="http://www.airwinsbigband.com/">AIRWIN's Big Band</a></p>
+                    <p><a href="http://www.blaskapelle-nordsued.com/">Blaskapelle Nord Süd</a></p>
                 </article>
             </main>
         </div>
