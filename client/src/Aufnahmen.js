c6b218899eabedb222b44d04822f2ec4f5ad8d05
Juwile <nadine.muelhauser@gmail.com>
2021-01-02 12:25:25 +0100

    aufnahmen

--- client/src/Aufnahmen.js
+++ client/src/Aufnahmen.js
@@ -11,7 +11,13 @@ export default function Aufnahmen() {
                   <div class="flex-container">
                     <div>
                       <h2>Schwur – Sandee</h2>
-                      <iframe src="https://www.facebook.com/plugins/video.php?height=300&href=https%3A%2F%2Fwww.facebook.com%2Fpurple.d.music%2Fvideos%2F2259385090794849%2F" width="300" height="168" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                      <div>
+                      <iframe class="hochformat" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpurple.d.music%2Fvideos%2F2259385090794849%2F" width="480" height="168" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                      </div>
+                    </div>
+                    <div>
+                      <h2>Letschte Atemzug – Trauffer</h2>
+                      <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpurple.d.music%2Fvideos%2F2009199149325390%2F" width="240" height="360" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                     </div>
                     <div>
                       <h2>Perfect – Ed Sheeran</h2>
@@ -21,10 +27,7 @@ export default function Aufnahmen() {
                       <h2>Halleluja – Leonard Cohen</h2>
                       <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpurple.d.music%2Fvideos%2F1611163915654490%2F" max-width="480" height="168" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true"></iframe>
                     </div>
-                    <div>
-                      <h2>Letschte Atemzug – Trauffer</h2>
-                      <iframe src="https://www.facebook.com/plugins/video.php?height=300&href=https%3A%2F%2Fwww.facebook.com%2Fpurple.d.music%2Fvideos%2F2009199149325390%2F" max-width="300" height="168" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-                    </div>
+
                     <div>
                       <h2>Heiterefahne – Trauffer</h2>
                       <iframe max-width="480" height="168" src="https://www.youtube.com/embed/YkEoxxhtQi0" frameborder="0" allowfullscreen="true"></iframe>
